4de29b6de3b0315d0a76a972503f43565b612582
Maximiliano Pin <mxcpin@gmail.com>
2011-03-05 19:00:58 +0100

    LGPL license.

--- redmodel/models/attributes.py
+++ redmodel/models/attributes.py
@@ -1,8 +1,20 @@
-'''
-Created on 12/12/2010
+"""
+    Copyright (C) 2011 Maximiliano Pin
 
-@author: mad
-'''
+    Redmodel is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Redmodel is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Redmodel.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 class Attribute(object):
     def __init__(self, indexed = False, unique = False):
         self.indexed = indexed or unique
--- redmodel/models/base.py
+++ redmodel/models/base.py
@@ -1,8 +1,20 @@
-'''
-Created on 12/12/2010
+"""
+    Copyright (C) 2011 Maximiliano Pin
 
-@author: mad
-'''
+    Redmodel is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Redmodel is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Redmodel.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from redmodel import connection as ds
 from redmodel.containers import ListHandle, SetHandle
 from redmodel.models.attributes import Attribute, ReferenceField, ListField, SetField, Recursive
